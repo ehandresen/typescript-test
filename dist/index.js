fdf3cd1a6f4b204ac5804e2ba0f992ef7948da8a
ehandresen <eirik.horgen.andresen@gmail.com>
2024-05-13 16:15:31 +0200

    functions, types, enum

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,55 @@
 "use strict";
+// basic types
 let id = 5;
-console.log(`ID:`, id);
+let company = 'Eirik';
+let isPublished = true;
+let x = 'hello';
+x = true;
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, 'string'];
+// tuple
+let person = [1, 'string', true];
+// tuple array
+let employee;
+employee = [
+    [1, 'string'],
+    [2, 'string'],
+    [3, 'string'],
+];
+// union
+let productId = 22; // kan be a string or number
+// enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
+console.log(Direction1.Left); // --> 0
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+console.log(Direction2.Up); // --> Up
+const user = {
+    id: 1,
+    name: 'John',
+};
+// type assertion
+let cid = 1;
+let customerId = cid; // now customerId is a number
+let personId = cid; // alternative syntax
+// functions
+function addNum(x, y) {
+    return x + y;
+}
+// void
+function log(message) {
+    // message can be string or number, return value nothing (void)
+    console.log(message);
+}
+log('hello');
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,70 @@
+// basic types
 let id: number = 5;
+let company: string = 'Eirik';
+let isPublished: boolean = true;
+let x: any = 'hello';
+x = true;
 
-console.log(`ID:`, id);
+let ids: number[] = [1, 2, 3, 4, 5];
+let arr: any[] = [1, true, 'string'];
+
+// tuple
+let person: [number, string, boolean] = [1, 'string', true];
+// tuple array
+let employee: [number, string][];
+
+employee = [
+  [1, 'string'],
+  [2, 'string'],
+  [3, 'string'],
+];
+
+// union
+let productId: string | number = 22; // kan be a string or number
+
+// enum
+enum Direction1 {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+console.log(Direction1.Left); // --> 0
+
+enum Direction2 {
+  Up = 'Up',
+  Down = 'Down',
+  Left = 'Left',
+  Right = 'Right',
+}
+
+console.log(Direction2.Up); // --> Up
+
+// objects
+type User = {
+  id: number;
+  name: string;
+};
+
+const user: User = {
+  id: 1,
+  name: 'John',
+};
+
+// type assertion
+let cid: any = 1;
+let customerId = <number>cid; // now customerId is a number
+let personId = cid as number; // alternative syntax
+
+// functions
+function addNum(x: number, y: number): number {
+  return x + y;
+}
+
+// void
+function log(message: string | number): void {
+  // message can be string or number, return value nothing (void)
+  console.log(message);
+}
+
+log('hello');
